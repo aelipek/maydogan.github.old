e26d63bd216f5649451baf00e2919e06556fa7a2
Meltem Aydoğan <meltem.aydogan@bil.omu.edu.tr>
2012-02-03 00:03:39 +0200

    .

---
+++ chrome/slide/ikon.js
@@ -0,0 +1,2 @@
+<script type="text/javascript" src="http://cdn.widgetserver.com/syndication/subscriber/InsertWidget.js"></script><script type="text/javascript">if (WIDGETBOX) WIDGETBOX.renderWidget('123e20cf-957e-4954-8cd4-659fd12906f6');</script>
+<noscript>Get the <a href="http://www.widgetbox.com/widget/suzy-cat">Suzy Cat</a> widget and many other <a href="http://www.widgetbox.com/">great free widgets</a> at <a href="http://www.widgetbox.com">Widgetbox</a>! Not seeing a widget? (<a href="http://support.widgetbox.com/">More info</a>)</noscript>
